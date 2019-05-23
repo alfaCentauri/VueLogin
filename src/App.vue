a2bcc00bacc463981fd5ed63def2aa69fe474189
Ricardo Presilla <ricardopresilla@gmail.com>
2019-05-23 11:07:15 -0400

    Agregado archivos app.vue y main.js

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <login></login>
+  </div>
+</template>
+
+<script>
+import login from './components/login.vue'
+
+export default {
+  name: 'app',
+  components: {
+    login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+}
+</style>
